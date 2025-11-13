49f79d17abef538bcc328ac3139e366dcbde361c
daws-100s <narayanalaxmanmangena@gmail.com>
2025-11-13 16:07:03 +0530

    tfvars

---
+++ tfvars/ec2.tf
@@ -0,0 +1,45 @@
+resource "aws_instance" "expense" {
+    for_each = var.instances
+    ami = "ami-0733cbac1dcca0be4"
+    instance_type = each.value
+    vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+
+    tags = merge(
+        var.common_tags,
+        var.tags,
+        {
+            Name = each.key
+        }
+    )
+
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh_${var.environment}"
+    description = "allow port number 22"
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    tags = merge(
+        var.common_tags,
+        var.tags,
+        {
+            Name = "allow_ssh_${var.environment}"
+        }
+    )
+  
+}
+
---
+++ tfvars/variables.tf
@@ -0,0 +1,29 @@
+variable "instances" {
+    type = map
+
+}
+
+variable "domain_name" {
+    default = "daws81s.online"
+}
+
+variable "zone_id" {
+    default = "Z0951733247NJS9XF1LII"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Terraform = "true"
+    }
+  
+}
+
+variable "tags" {
+    default = map
+  
+}
+
+variable "environment" {
+  
+}
